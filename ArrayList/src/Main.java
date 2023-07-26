663abf3188cf456960ce90dab7525f571530147a
whiHak <betse1111111111@gmail.com>
2023-07-26 20:11:27 +0300

    added main file for arraylist

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String arg[]){
+        ArrayList arr = new ArrayList(1);
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+
+        System.out.println("The 5th element is: " + arr.get(4));
+
+    }
+}
+
