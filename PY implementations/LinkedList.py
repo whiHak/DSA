bef9b6862d13cf4652461c1aa0db8993eca7f015
whiHak <betse1111111111@gmail.com>
2023-07-28 14:17:12 +0300

    added python implementation

---
+++ PY implementations/LinkedList.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+class Linked:
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def is_empty(self):
+        return self.first is None
+
+    def appened(self, item):
+        node = Node(item)
+        if self.is_empty():
+            self.first = self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+
+    def prepend(self, item):
+        node = Node(item)
+        if self.is_empty():
+            self.first = self.last = node
+        else:
+            node.next = self.first
+            self.first = node
+
+    def delete(self, item):
+        if self.is_empty():
+            return "No nodes to delete"
+        else:
+            if self.first.item == item:
+                self.first = self.first.next
+                return
+            current = self.first
+            while current.next.item != item:
+                current = current.next
+            
+            current.next = current.next.next
+            
+    def display(self):
+        node = self.first
+        while node:
+            print(node.item, end=" -> ")
+            node = node.next
+
+
+lists = Linked()
+lists.appened(30)
+lists.appened(40)
+lists.appened(50)
+lists.prepend(20)
+lists.delete(40)
+lists.display()
---
+++ PY implementations/Stack.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, item):
+        self.stack.append(item)
+    def pop(self):
+        pop_item = self.stack.pop()
+        return pop_item
+    def display(self):
+        print(self.stack)
+
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.push(50)
+stack.pop()
+stack.display()
